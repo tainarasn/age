f995aad74e1b993b65252035f17d01de9ed6d6e5
Tainara Novaes <tainarasn.sba@gmail.com>
2023-12-03 12:04:06 -0300

    feat: adde index

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-
+import App from "./src/App"
+import reportWebVitals from "./src/reportWebVitals"
 
 ReactDOM.render(
     <React.StrictMode>
-       <App/>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 )
